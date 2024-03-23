066edd370218259726712cfd53b643cdb501274d
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-23 22:50:58 +0000

    Change PageSection component's padding-size

--- src/components/PageSection/index.tsx
+++ src/components/PageSection/index.tsx
@@ -7,5 +7,5 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 
 
 export  default function PageSection ({className,children , ...props}:Props){
-    return (<section className={cn('container pt-16 lg:pt-24 pb-12',className)}  {...props}>{children}</section>)
+    return (<section className={cn('container lg:px-[4rem] xl:px-[10rem] pt-16 lg:pt-24 pb-12',className)}  {...props}>{children}</section>)
 }
