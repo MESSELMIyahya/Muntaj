a01552a78865d61d75c99a1b60fb1683d167a693
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-24 01:01:29 +0000

    Add CategoriesPart component for categories buttons

---
+++ src/components/CategoriesPart/index.tsx
@@ -0,0 +1,43 @@
+import SectionPart from "../SectionPart";
+import { Button } from "../ui/button";
+import CategoryItem from "../CategoryItem";
+import { IconType } from "react-icons/lib";
+import Link from "next/link";
+
+
+type ItemType = {
+    Icon: IconType;
+    title: string;
+    link: string;
+}
+
+
+interface Props {
+    items: ItemType[]
+}
+
+
+export default function CategoriesSection({ items }: Props) {
+    return (<SectionPart className="w-full flex justify-center">
+
+        <div className="w-2/3 flex justify-center gap-6 flex-wrap">
+
+            {
+                items.map(e =>
+
+                    <div className="max-w-fit flex flex-col gap-2 items-center">
+
+                        <Button asChild variant='ghost' className="w-fit h-fit p-0 rounded-full">
+                            <Link href={e.link}>
+                                <CategoryItem size="lg" Icon={e.Icon} />
+                            </Link>
+                        </Button>
+                        <p className="text-base font-medium text-accent-foreground">{e.title}</p>
+                    </div>
+                )
+            }
+
+        </div>
+
+    </SectionPart>)
+}
