f8c59516c5251071f9d848aab9dd61c3bfe7fbdb
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-24 00:57:49 +0000

    Add MainAd, SquareAd ,LineAd components for advertising

---
+++ src/components/MainAd/index.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+
+interface Props {
+    ImageSrc:string,
+    owner:string,
+}
+
+export default function MainAD ({ImageSrc,owner}:Props){
+
+
+    return (<div className="w-full relative overflow-hidden select-none h-[8em] sm:h-[12em] md:h-[14em] lg:h-[18em] bg-card rounded-xl border p-2  shadow-sm">
+        <div className="absolute top-0 left-0 w-full h-full"></div>
+        <span className="absolute left-4 bottom-4 rounded-md px-2 py-1 bg-card/20 border border-border/10 backdrop-blur-sm text-xs md:text-sm text-accent-foreground">{owner}</span>
+        <Image alt='AD' className="w-full h-full rounded-xl object-center" src={ImageSrc} width={900} height={300}  />
+    </div>);
+}
---
+++ src/components/SquareAd/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+interface Props {
+    ImageSrc:string,
+    owner:string,
+}
+
+export default function SquareAD ({ImageSrc,owner}:Props){
+
+    return (<div className="relative overflow-hidden h-[12em] select-none bg-card rounded-xl border p-2  shadow-sm">
+        <div className="absolute top-0 left-0 w-full h-full"></div>
+        <span className="absolute left-4 bottom-4 rounded-md px-2 py-1 bg-card/20 border border-border/10 backdrop-blur-sm text-xs md:text-sm text-accent-foreground">{owner}</span>    
+        <Image alt="ad" className="w-full h-full rounded-xl object-center" src={ImageSrc} width={600} height={500}  />
+    </div>);
+}
