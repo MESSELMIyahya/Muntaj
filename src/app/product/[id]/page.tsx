d4fe2c31cc3db92acf00549a446cda021e1a7d7a
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-03-26 23:34:30 +0200

    STYLE_ENHANCE : for product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -4,76 +4,109 @@ import { HiStar } from 'react-icons/hi';
 
 import Image from 'next/image';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 const imgURL =
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0LgIPwB4gjYlOy5_YtiC7GSU5VJQVBgwG2w&s';
 
 export default function page() {
   return (
-    <div className="mt-16 max-w-7xl m-auto space-y-10">
-      <div className="grid gap-8 grid-cols-3">
-        <div className="col-span-1 bg-yellow-200">
+    <div className="mt-16 max-w-6xl m-auto space-y-10">
+      <Card className="grid gap-8 grid-cols-3">
+        <CardHeader className="col-span-1 bg-yellow-200">
           <Image src={imgURL} width={800} height={800} alt="product" />
-        </div>
-        <div className="col-span-2 space-y-4">
-          <Badge>الكترونيات</Badge>
-          <h1 className="text-2xl">
-            وحدة تحكم بلاي ستيشن 5 (إصدار القرص) مع وحدة التحكم
-          </h1>
-          <p className="text-lg">
-            منتج يتميز بسرعة عالية - استغل قوة وحدة المعالجة المركزية المخصصة
-            وبطاقة الرسومات ومحرك الأقراص SSD مع وحدات الإدخال والإخراج المدمجة
-            التي تعمل على إعادة كتابة القواعد التي يمكن لجهاز الألعاب بلايستيشن
-            فعلها. ألعاب مذهلة - استمتع برسومات مذهلة واستمتع بتجربة ميزات PS5
-            الجديدة. انغماس مذهل - اكتشف تجربة ألعاب أعمق مع دعم التغذية الراجعة
-            اللمسية والمحفزات التكيفية وتقنية الصوت ثلاثية الأبعاد. استمتع بلعب
-            سلس ومرن وذي معدل إطارات عالٍ يصل إلى 120 إطار في الثانية للألعاب
-            المتوافقة. بفضل تقنية HDR tv،
-          </p>
-          <div className="flex items-center gap-6">
-            <Button className="w-full">تواصل</Button>
-            <Button variant="outline" size="icon">
-              <HiStar className="size-6 transition-colors text-neutral-300" />
-            </Button>
-          </div>
-        </div>
-      </div>
+        </CardHeader>
 
-      <div className="grid grid-cols-3">
-        <div className="col-span-1 space-y-2">
-          <h2 className="text-2xl font-semibold">التقييم العام</h2>
-          <h3 className="text-xl font-semibold">4</h3>
-          <div className="flex gap-1">
-            <HiStar className="size-6 transition-colors text-amber-500" />
-            <HiStar className="size-6 transition-colors text-amber-500" />
-            <HiStar className="size-6 transition-colors text-amber-500" />
-            <HiStar className="size-6 transition-colors text-amber-500" />
-            <HiStar className="size-6 transition-colors text-neutral-300" />
-          </div>
-          <p>بناءً على 8179 تقييمات</p>
-        </div>
+        <CardContent className="col-span-2 space-y-4 mt-4">
+          <ProductContent />
+        </CardContent>
+      </Card>
 
-        <div className="space-y-4 col-span-2">
+      <Card className="grid grid-cols-3 p-2 gap-2">
+        <Card className="col-span-1 space-y-2">
+          <CardHeader>
+            <h2 className="text-2xl font-semibold">التقييم العام</h2>
+            <h3 className="text-xl font-semibold">4</h3>
+          </CardHeader>
+          <CardContent>
+            <CardDescription>
+              <RatingStar />
+              <p>بناءً على 8179 تقييمات</p>
+            </CardDescription>
+          </CardContent>
+        </Card>
+
+        <Card className="col-span-2">
+          <CardHeader>
+            <h2 className="text-2xl">اراء المستخدمين</h2>
+          </CardHeader>
           <UserComment />
           <UserComment />
           <UserComment />
           <UserComment />
-        </div>
-      </div>
+        </Card>
+      </Card>
     </div>
+  );
+}
+
+function ProductContent() {
+  return (
+    <>
+      <Badge>الكترونيات</Badge>
+      <h1 className="text-2xl">
+        وحدة تحكم بلاي ستيشن 5 (إصدار القرص) مع وحدة التحكم
+      </h1>
+      <p className="text-lg">
+        منتج يتميز بسرعة عالية - استغل قوة وحدة المعالجة المركزية المخصصة وبطاقة
+        الرسومات ومحرك الأقراص SSD مع وحدات الإدخال والإخراج المدمجة التي تعمل
+        على إعادة كتابة القواعد التي يمكن لجهاز الألعاب بلايستيشن فعلها. ألعاب
+        مذهلة - استمتع برسومات مذهلة واستمتع بتجربة ميزات PS5 الجديدة. انغماس
+        مذهل - اكتشف تجربة ألعاب أعمق مع دعم التغذية الراجعة اللمسية والمحفزات
+        التكيفية وتقنية الصوت ثلاثية الأبعاد. استمتع بلعب سلس ومرن وذي معدل
+        إطارات عالٍ يصل إلى 120 إطار في الثانية للألعاب المتوافقة. بفضل تقنية
+        HDR tv،
+      </p>
+      <div className="flex items-center gap-6">
+        <Button className="w-full">تواصل</Button>
+        <Button variant="outline" size="icon">
+          <HiStar className="size-6 transition-colors text-neutral-300" />
+        </Button>
+      </div>
+    </>
   );
 }
 
 function UserComment() {
   return (
-    <div className="flex gap-4 items-center">
+    <CardContent className="flex gap-4 items-center">
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
       <div>
-        <h2 className="text-xl">محمد عادل</h2>
-        <p>المنتج في غاية الروعة, استخدامه سهل وجودة الخامات عالية</p>
+        <CardTitle className="text-xl">محمد عادل</CardTitle>
+        <CardDescription>
+          المنتج في غاية الروعة, استخدامه سهل وجودة الخامات عالية
+        </CardDescription>
       </div>
+    </CardContent>
+  );
+}
+
+function RatingStar() {
+  return (
+    <div className="flex gap-1">
+      <HiStar className="size-6 transition-colors text-amber-500" />
+      <HiStar className="size-6 transition-colors text-amber-500" />
+      <HiStar className="size-6 transition-colors text-amber-500" />
+      <HiStar className="size-6 transition-colors text-amber-500" />
+      <HiStar className="size-6 transition-colors text-neutral-300" />
     </div>
   );
 }
