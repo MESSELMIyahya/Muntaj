e9a49072098229e6d2752b696b38ec447e35b451
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-29 17:18:46 +0100

    Chagne all get data function to work with fetch

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -21,8 +21,8 @@ async function getProducts() : Promise<ProductType[]|null> {
 
 async function getProductsBySearch(keywords:string) : Promise<ProductType[]|null> {
   try{
-    const res = await AxiosReq.get('/api/v1/product?search='+keywords)
-    const prods : ProductType[] = res.data.data; 
+    const res = await fetch(baseURL+'/api/v1/product?search='+keywords,{cache:"no-store"})
+    const prods : ProductType[] = (await res.json()).data; 
     return prods
   }catch(err){
     console.log(err);
@@ -32,8 +32,11 @@ async function getProductsBySearch(keywords:string) : Promise<ProductType[]|null
 
 async function getProductsByCategory(id:string,limit:number=50) : Promise<ProductType[]|null> {
   try{
-    const res = await AxiosReq.get('/api/v1/product'+'?category='+id+'&limit='+limit);
-    const prods : ProductType[] = res.data.data; 
+    // const res = await AxiosReq.get('/api/v1/product'+'?category='+id+'&limit='+limit);
+    // const prods : ProductType[] = res.data.data;
+    const res = await fetch(baseURL+'/api/v1/product'+'?category='+id+'&limit='+limit,{cache:"no-store"})
+    const prods : ProductType[] = (await res.json()).data;  
+
     return prods
   }catch(err){
     console.log(err);
@@ -43,8 +46,10 @@ async function getProductsByCategory(id:string,limit:number=50) : Promise<Produc
 
 async function getProductsLimit(num:number=20) : Promise<ProductType[]|null> {
   try{
-    const res = await AxiosReq.get('/api/v1/product'+'?limit='+num);
-    return res.data.data;
+    // const res = await AxiosReq.get('/api/v1/product'+'?limit='+num);
+    const res = await fetch(baseURL+'/api/v1/product'+'?limit='+num,{cache:"no-store"})
+    const prods : ProductType[] = (await res.json()).data; 
+    return prods;
   }catch(err){
     console.log(err);
     return null ;
@@ -53,8 +58,10 @@ async function getProductsLimit(num:number=20) : Promise<ProductType[]|null> {
 
 async function getProduct(id: string): Promise<ProductType|null> {
   try{
-    const res = await AxiosReq.get('/api/v1/product/'+id)
-    return res.data.data;
+    // const res = await AxiosReq.get('/api/v1/product/'+id)
+    const res = await fetch(baseURL+'/api/v1/product/'+id,{cache:"no-store"})
+    const prod : ProductType = (await res.json()).data; 
+    return prod ;
   }catch(err){
     console.log(err);
     return null ;
