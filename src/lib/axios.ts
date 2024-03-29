6346b5dbc795889583d698abe437212b614c0dbd
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-29 10:46:08 +0100

    Try fix axios req

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -7,7 +7,7 @@ interface aXType extends CreateAxiosDefaults  {
 const axiosConfig : aXType = {
     withCredentials:true,
     baseURL:process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:5000',
-    headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
+    // headers: {'Access-Control-Allow-Origin': '*'},
     // @ts-ignore
 	credentials: 'include',
     cache: 'no-store',
