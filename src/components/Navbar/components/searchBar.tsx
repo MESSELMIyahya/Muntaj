ad282da296496b39f00b74574a91fbf845d9a3a8
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-29 08:04:15 +0100

    Add Searchbar component  to the Navbar

--- src/components/Navbar/components/searchBar.tsx
+++ src/components/Navbar/components/searchBar.tsx
@@ -11,7 +11,7 @@ export default function SearchBar (){
     return  (<div className='w-3/6 flex items-center relative'>
         <Input value={text} onChange={(e)=>setText(e.target.value)} className='w-full py-2 px-3 rounded-r-full' placeholder="ابحث عن" />
         <Button asChild className="rounded-l-full text-sm">
-            <Link href={`/search/${text}`}>
+            <Link href={`/search/${text} `}>
                 بحث
             </Link>
         </Button>
