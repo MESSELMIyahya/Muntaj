bb01f3167a15e0d0df0797f84069c5cd21565ddc
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-29 08:11:57 +0100

    Update style and add uesContact hook

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -10,6 +10,7 @@ import GetCountryName from "@/lib/country";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 import CategoryName from "../CategoryName";
+import useContact from "@/hooks/useContact";
 
 interface Props {
   image: string;
@@ -23,6 +24,19 @@ interface Props {
   store: {
     name: string;
     id: string;
+    contact:{
+      phoneNumbers: string[];
+      email: string;
+      website?: string; // => this is optional
+      socialMedia: {
+        // all of the sm account are optional
+        facebook?: string;
+        instagram?: string;
+        linkedIn?: string;
+        twitter?: string;
+        youtube?: string;
+      };
+    };
   };
 }
 
@@ -42,18 +56,18 @@ export default function ProductCard({
   const handleRate = () => {
     setIRated((e) => !e);
   };
+  const {contact} = useContact()
 
   return (
-    <div className="p-4 rounded-xl bg-card duration-300 hover:duration-300 transition-colors hover:transition-colors hover:bg-accent hover:shadow-sm border mb-6">
+    <div className="w-full md:w-[21em] p-4 rounded-xl bg-card duration-300 hover:duration-300 transition-colors hover:transition-colors hover:bg-accent hover:shadow-sm border ">
       <div className="w-full h-[16em] relative bg-white border overflow-hidden rounded-xl mb-4 ">
         <Image
           className="w-full h-full object-contain"
           src={image}
-          width={500}
-          height={600}
-          alt="prodct"
-        />
-
+          loading="lazy"
+          width={200}
+          height={300}
+          alt="product"/>
         <div className="absolute right-2 bottom-2 rounded-md p-1 bg-card font-medium text-sm select-none text-card-foreground border flex items-center gap-1">
           {rating}
           <HiStar className="size-4 text-amber-500" />
@@ -63,20 +77,20 @@ export default function ProductCard({
       <div className="w-full">
         <div className="w-full flex items-center justify-between mb-2">
           <div className="flex items-center gap-2">
-            <Badge className="flex items-center gap-1 rounded-sm text-sm ">
+            <Badge className="flex items-center gap-1 rounded-sm ">
               <CategoryName ID={category} />
             </Badge>
 
             <Badge
               variant="outline"
-              className="flex text-sm items-center gap-1 rounded-sm"
+              className="flex items-center gap-1 rounded-sm"
             >
               {GetCountryName(country)}
               <CountryFlag country={country} className="w-4" />
             </Badge>
           </div>
 
-          <Link href={`stores/${store.id}`}>
+          <Link href={`/store/${store.id}`}>
             <Badge
               variant="secondary"
               className="w-[8em] group relative flex justify-center select-none cursor-pointer overflow-hidden rounded-md text-sm"
@@ -88,18 +102,18 @@ export default function ProductCard({
         </div>
 
         <Link
-          href={`product/${id}`}
+          href={`/product/${id}`}
           className="text-xl transition-colors hover:text-primary line-clamp-2 text-accent-foreground font-medium mb-2"
         >
           {title}
         </Link>
-        <p className="text-sm line-clamp-4 text-muted-foreground mb-2">
+        <p className="text-sm line-clamp-2 text-muted-foreground mb-2">
           {description}
         </p>
       </div>
 
       <div className="w-full flex gap-2 items-center">
-        <Button className="flex-1">تواصل</Button>
+        <Button onClick={()=>contact({name:store.name,info:store.contact})} className="flex-1">تواصل</Button>
         <Button onClick={handleRate} variant="outline" size="icon">
           <HiStar
             className={cn(
