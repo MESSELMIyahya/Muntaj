7046cec3b6d97a56f4b315a83d3516978a2380db
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-31 03:03:56 +0100

    Add ProductCardSkeleton component (it's sekeleton like productCard)

---
+++ src/components/ProductCard/ProductCardSkeleton.tsx
@@ -0,0 +1,6 @@
+import { Skeleton } from "../ui/skeleton";
+
+
+export default function ProductCardSkeleton (){
+    return (<Skeleton className="w-full h-[29.04em] md:w-[21em] p-4 rounded-xl" />)
+}
